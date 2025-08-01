d17bb00102116e999a1e601fc12a6ea715e5ab53
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-08-01 10:34:00 +0530

    header

--- Easystay/home/views.py
+++ Easystay/home/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+from django.template import loader
+
+from django.http import HttpResponse
+
+from django.views.generic import DetailView, DeleteView, UpdateView,CreateView
+
+
 # Create your views here.
+def home(request):
+    template= loader.get_template('home.html')
+    context ={
+        
+    }
+    return HttpResponse(template.render(context,request))
+
+def about(request):
+    template= loader.get_template('about.html')
+    context={
+
+    }
+    return HttpResponse(template.render(context,request))
+
+def contact(request):
+    template= loader.get_template('contact.html')
+    context={
+    }
+    return HttpResponse(template.render(context,request))
